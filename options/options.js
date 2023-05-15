cc24da0b4aaf5431ace1d2df02b40256e72f254b
Matthew Miller <mattmiller@acsouth.com>
2023-05-15 13:06:38 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "royal-firefly-40706",
-  url: "https://royal-firefly-40706.botics.co",
-  api: "https://royal-firefly-40706.botics.co/api/v1"
-}
+  "name": "royal-firefly-40706",
+  "url": "https://royal-firefly-40706.botics.co",
+  "api": "https://royal-firefly-40706.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
